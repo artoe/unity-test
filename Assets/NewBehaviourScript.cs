971a352d42b13ec6ca34a6595cb6c09a146703f3
artoe <arto.esko@gmail.com>
2015-04-02 19:38:29 +0300

    testing

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public static float fallSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+	}
+}
