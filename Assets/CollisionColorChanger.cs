59d85c701ba5babbf84fbdd2bc48a88a6bd3ecf1
Arto Esko <arto.esko@sysart.fi>
2015-04-07 09:20:16 +0300

    testing...

---
+++ Assets/CollisionColorChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionColorChanger : MonoBehaviour {
+
+	Color[] c = { Color.cyan, Color.green, Color.magenta, Color.yellow };
+	static int i = 0;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		MeshRenderer x = col.gameObject.GetComponent<MeshRenderer> ();
+		x.material.color =c[(i++) % c.Length];
+	}
+}
---
+++ Assets/InitialSpin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialSpin : MonoBehaviour {
+	void Start ()
+	{
+		GetComponent<Rigidbody>().AddTorque(12,-56, 14, ForceMode.Impulse);
+	}
+
+	// Use this for initialization
+	void Update () {
+
+	}
+}
